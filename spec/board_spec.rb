073611dbca210bed4e6f37ca30b1ee5b207986df
Mariel Hoepelman <marielhoepelman@gmail.com>
2017-12-16 21:03:21 +0000

    automatically backed up by learn

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,7 @@
 describe "lib/board.rb" do
   it 'defines a local variable `board`' do
     board = get_variable_from_file("./lib/board.rb", "board")
-    binding.pry
+    byebug
 
     expect(board).to_not be_nil
   end
